01bf65732f846ccdfb212fffb0dfb3220bb3ace4
Elijah <ilyasmolnikov567@gmail.com>
2023-10-01 15:39:09 +0300

    Intial commit

---
+++ simple_maven_project/src/test/java/org/viciousxerra/simple_maven_project/AppTest.java
@@ -0,0 +1,17 @@
+package org.viciousxerra.simple_maven_project;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    public void shouldAnswerWithTrue()
+    {
+        
+    }
+}
