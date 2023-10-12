2d5c608ddaa2a9eba447f55e8df5de866668f92d
Elijah <ilyasmolnikov567@gmail.com>
2023-10-13 01:08:25 +0300

    Test refactoring

--- selenium_part2/src/test/java/org/viciousxerra/selenium_part2/AppTest.java
+++ selenium_part2/src/test/java/org/viciousxerra/selenium_part2/AppTest.java
@@ -2,13 +2,16 @@ package org.viciousxerra.selenium_part2;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,19 +21,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.net.*;
 import java.time.Duration;
 import java.util.List;
 
-/*
- * Проверить надписи в незаполненных полях каждого варианта оплаты услуг: услуги связи,
- * домашний интернет, рассрочка, задолженность;
- * 
- * Для варианта «Услуги связи» заполнить поля в соответствии с пререквизитами из предыдущей
- * темы, нажать кнопку «Продолжить» и в появившемся окне проверить корректность отображения
- * суммы (в том числе на кнопке), номера телефона, а также надписей в незаполненных полях для
- * ввода реквизитов карты, наличие иконок платёжных систем.
- */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ChromeBrowserTest {
 	private static String URL = "https://www.mts.by";
 	private static WebDriver driver;
@@ -39,9 +33,10 @@ class ChromeBrowserTest {
 	static void setUp() {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10L));
+		driver.manage().window().maximize();
 		driver.get(URL);
 	}
-	
+		
 	@AfterAll
 	static void teardown() {
 		driver.quit();
@@ -57,6 +52,7 @@ class ChromeBrowserTest {
 	}
 	
 	@ParameterizedTest
+	@Order(1)
 	@MethodSource("testDataProvider")
 	@DisplayName("Check block signs")
 	void testText(int type, String expectedTypeSign, String expectedMoneyValueSign, String expectedEmailSign) {
@@ -70,10 +66,90 @@ class ChromeBrowserTest {
 		.click(driver.findElement(By.xpath(String.format("//ul[@class = 'select__list']/li[%d]", type))))
 		.build()
 		.perform();
+		
 		//Then
-		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[1]/input")).getDomProperty("placeholder")).isEqualTo(expectedTypeSign);
-		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[2]/input")).getDomProperty("placeholder")).isEqualTo(expectedMoneyValueSign);
-		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[3]/input")).getDomProperty("placeholder")).isEqualTo(expectedEmailSign);
+		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[1]/input"))
+				.getDomProperty("placeholder")).isEqualTo(expectedTypeSign);
+		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[2]/input"))
+				.getDomProperty("placeholder")).isEqualTo(expectedMoneyValueSign);
+		assertThat(driver.findElement(By.xpath("//form[@class = 'pay-form opened']/div[3]/input"))
+				.getDomProperty("placeholder")).isEqualTo(expectedEmailSign);
+		
+	}
+	
+	@Test
+	@Order(2)
+	@DisplayName("Check phone payment form")
+	void testForm() {
+		//Given
+		String tel = "297777777";
+		String paymentValue = "10";
+		
+		//When
+		Actions action = new Actions(driver);
+		action
+		.scrollToElement(driver.findElement(By.xpath("//button[@class = 'select__header']")))
+		.click(driver.findElement(By.xpath("//button[@class = 'select__header']")))
+		.pause(Duration.ofSeconds(5L))
+		.scrollToElement(driver.findElement(By.xpath("//ul[@class = 'select__list']/li[1]")))
+		.click(driver.findElement(By.xpath("//ul[@class = 'select__list']/li[1]")))
+		.build()
+		.perform();
+		
+		driver.findElement(By.xpath("//input[@class = 'phone']")).sendKeys(tel);
+		driver.findElement(By.xpath("//input[@class = 'total_rub']")).sendKeys(paymentValue);
+		
+		JavascriptExecutor exec = (JavascriptExecutor)driver;
+		exec.executeScript("arguments[0].click();", driver.findElement(By.cssSelector("#pay-connection > button")));
+		new WebDriverWait(driver, Duration.ofSeconds(20L)).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
+		
+		String actualPrice = driver.findElement(By.className("header__payment-amount"))
+				.getDomProperty("textContent");
+		String actualPriceButton = driver.findElement(
+				By.cssSelector("body > app-root > div > div > app-payment-container > section > app-card-page > div > div.card-page__card > button"))
+				.getDomProperty("textContent");
+		String actualTelInfo = driver.findElement(By.className("header__payment-info"))
+				.getDomProperty("textContent");
+
+		//Then
+		assertThat(actualPrice.contains("10.00 BYN")).isEqualTo(true);
+		assertThat(actualPriceButton.contains("10.00 BYN")).isEqualTo(true);
+		assertThat(actualTelInfo.contains("375297777777")).isEqualTo(true);
+		
+		//When
+		boolean isPresent = true;
+		List<WebElement> logoList = driver.findElements(
+				By.cssSelector("body > app-root > div > div > app-payment-container > section > app-card-page > "
+						+ "div > div.card-page__card > app-card-input > form > div.card.ng-tns-c52-0 > "
+						+ "div:nth-child(1) > app-input > div > div > div.icons-container.ng-tns-c46-1 > div > div > img"));
+		assertThat(logoList.size()).isPositive();
+		
+		for(WebElement x : logoList) {
+			if(!x.isDisplayed()) {
+				isPresent = false;
+				break;
+			}
+		}
+		
+		//Then
+		assertThat(isPresent).isEqualTo(true);
+		
+		//When
+		String actualCardNumSign = driver.findElement(By.xpath("//app-input/div/div/div[1]/label"))
+				.getDomProperty("textContent");
+		String actualValidDate = driver.findElement(By.xpath("//app-card-input/form/div[1]/div[2]/div[1]/app-input/div/div/div[1]/label"))
+				.getDomProperty("textContent");
+		String CVCSign = driver.findElement(By.xpath("//app-card-input/form/div[1]/div[2]/div[3]/app-input/div/div/div[1]/label"))
+				.getDomProperty("textContent");
+		String userFirstAndLastName = driver.findElement(By.xpath("//app-card-input/form/div[1]/div[3]/app-input/div/div/div[1]/label"))
+				.getDomProperty("textContent");
+		
+		//Then
+		assertThat(actualCardNumSign).isEqualTo("Номер карты");
+		assertThat(actualValidDate).isEqualTo("Срок действия");
+		assertThat(CVCSign).isEqualTo("CVC");
+		assertThat(userFirstAndLastName).isEqualTo("Имя держателя (как на карте)");
+		
 	}
 		
 }
